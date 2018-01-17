27b34151708340b65e13bdb291fc248b82aa8cbf
Ayr3s00 <ricardo.ayres@outlook.com>
2018-01-17 04:03:18 -0200

    updated function calls and randomized generation

--- ffgen.c
+++ ffgen.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "geom.h"
 #include "ff.h"
-#include "sierp.h"
+#include "chaos.h"
 
 
 int main() {
+	/* unsigned char *seed = malloc(sizeof(char));
+	fgets(seed, sizeof(int), stdin);
+	srand((unsigned int) *seed);
+	*/
+
+	srand(time(NULL));
+
 	int width = 1440;
 	int height = 900;
 	if (width < 1 | height < 1) {
@@ -17,13 +25,15 @@ int main() {
 
 	FarbFeld *picture = FF_init(width, height);
 
-	int n = 300000;
+	int n = 1000000;
 	Point *pos = malloc(n*sizeof(Point));
 	if (!pos) {
 		fprintf(stderr, "ffgen: oops, can't allocate pos!\n");
 	}
 
-	pos = sierp(n);
+	int verts = 3 + (rand()%5); /* verts in [3,7] */
+	int rule = rand()%3;
+	pos = chaos(n, verts, rule);
 	
 	FF_plot_pos(picture, pos, n);
 
